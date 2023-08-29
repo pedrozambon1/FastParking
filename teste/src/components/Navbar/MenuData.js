289824922b8f572544be65f1517c26838c0181f4
ItsNotNatan <127156404+ItsNotNatan@users.noreply.github.com>
2023-08-29 20:10:15 -0300

    Add files via upload

---
+++ teste/src/components/Navbar/MenuData.js
@@ -0,0 +1,31 @@
+export const MenuData = [
+    {
+        title:"Home",
+        cName: "nav-links",
+        icon: "fa-solid fa-house-user ",
+        router:"/Home"
+    },
+    {
+        title:"Sobre",
+        cName: "nav-links",
+        icon: "fa-solid fa-circle-info ",
+        router:"/Sobre"
+    },
+    {
+        title:"Serviço",
+        cName: "nav-links",
+        icon: "fa-solid fa-briefcase ",
+        router:"/Servico"
+    },
+    {
+        title:"Câmeras",
+        cName: "nav-links",
+        icon: "fa-sharp fa-solid fa-camera",
+        router:"/Cameras"
+    },
+    {
+        title:"Sign Up",
+        cName: "nav-links-mobile",
+        router:"/Sign Up"
+    }
+]
---
+++ teste/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar'
+import AllCameras from '../components/AllCameras';
+
+export default function Home() {
+    
+    return (
+      <div className='App'>
+        <Navbar/>
+      </div>
+    );
+  }
+  
