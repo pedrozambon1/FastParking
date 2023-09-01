2ccc50dc68d5a2b0991a508c46bcb5d45cd2e690
Pedro Zamboni Branzani <134438541+pedrozambon1@users.noreply.github.com>
2023-09-01 12:27:59 -0300

    Menu bar (novo)
    Logo nova  e cores novas

---
+++ FastParking/teste/src/components/Navbar/Home.js
@@ -0,0 +1,13 @@
+import "./HomeStyles.css"
+
+function Home(){
+    return(<>
+    <div className="home">
+        
+    
+    </div>
+    </>
+    );
+}
+
+export default Home;
---
+++ FastParking/teste/src/components/Navbar/MenuData.js
@@ -0,0 +1,31 @@
+export const MenuData = [
+    {
+        title:"Real Time",
+        cName: "nav-links",
+        icon: "fa-solid fa-house-user ",
+        router:"/Home"
+    },
+    {
+        title:"Statistics",
+        cName: "nav-links",
+        icon: "fa-solid fa-circle-info ",
+        router:"/Sobre"
+    },
+    {
+        title:"Services",
+        cName: "nav-links",
+        icon: "fa-solid fa-briefcase ",
+        router:"/Servico"
+    },
+    {
+        title:"Settings",
+        cName: "nav-links",
+        icon: "fa-sharp fa-solid fa-camera",
+        router:"/Cameras"
+    },
+    {
+        title:"Sign Up",
+        cName: "nav-links-mobile",
+        router:"/Sign Up"
+    }
+]
